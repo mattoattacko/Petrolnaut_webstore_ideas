e61ddb7f61cec4d2faa42636d3a4ab669dd69b1b
unknown <matt.mcquain@gmail.com>
2021-04-18 21:05:52 -0700

    fixed CTA styling issue

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,13 +3,12 @@ module.exports = {
     purgeLayersByDefault: true,
     applyComplexClasses: true,
   },
-  purge: {
-    content: [
-      './pages/**/*.{js,ts,jsx,tsx}',
-      './sections/**/*.{js,ts,jsx,tsx}',
-      './components/**/*.{js,ts,jsx,tsx}',
-    ],
-  },
+  purge: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+    './blocks/**/*.{js,ts,jsx,tsx}',
+  ],
+  
   theme: {
     extend: {
       maxWidth: {

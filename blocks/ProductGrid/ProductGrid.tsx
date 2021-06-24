45071250858719afabad19b3fcc0473c1e5142ec
unknown <matt.mcquain@gmail.com>
2021-06-23 20:41:39 -0700

    trying to fix production grid

--- blocks/ProductGrid/ProductGrid.tsx
+++ blocks/ProductGrid/ProductGrid.tsx
@@ -1,28 +1,115 @@
-/** @jsxRuntime classic */
-/** @jsx jsx */
-import { jsx } from 'theme-ui'
-import { FC, useEffect, useState } from 'react'
-import { LoadingDots } from '@components/ui'
-import { Grid } from '@theme-ui/components'
-import { ProductCardProps } from '@components/common/ProductCard'
-import { ProductCardDemo, ProductCard } from '@components/common'
+// /** @jsxRuntime classic */
+// /** @jsx jsx */
+// import { jsx } from 'theme-ui'
+// import { FC, useEffect, useState } from 'react'
+// import { LoadingDots } from '@components/ui'
+// import { Grid } from '@theme-ui/components'
+// import { ProductCardProps } from '@components/common/ProductCard'
+// import { ProductCardDemo, ProductCard } from '@components/common'
 
+// import {
+//   getCollection,
+//   getProduct,
+// } from '@lib/shopify/storefront-data-hooks/src/api/operations-builder'
+// import builderConfig from '@config/builder'
+// interface HighlightedCardProps extends Omit<ProductCardProps, 'product'> {
+//   index: number
+// }
+
+// export interface ProductGridProps {
+//   products?: ShopifyBuy.Product[]
+//   productsList?: Array<{ product: string }>
+//   collection?: string | any // ShopifyBuy.Collection
+//   offset: number
+//   limit: number
+//   cardProps: Omit<ProductCardProps, 'product'>
+//   highlightCard?: HighlightedCardProps
+// }
+
+// export const ProductGrid: FC<ProductGridProps> = ({
+//   products: initialProducts,
+//   collection,
+//   productsList,
+//   offset = 0,
+//   limit = 10,
+//   cardProps,
+//   highlightCard,
+// }) => {
+//   const [products, setProducts] = useState(initialProducts || [])
+//   const [loading, setLoading] = useState(false)
+
+//   useEffect(() => {
+//     const getProducts = async () => {
+//       setLoading(true)
+//       const promises = productsList!
+//         .map((entry) => entry.product)
+//         .filter((handle: string | undefined) => typeof handle === 'string')
+//         .map(
+//           async (handle: string) => await getProduct(builderConfig, { handle })
+//         )
+//       setProducts(await Promise.all(promises))
+//       setLoading(false)
+//     }
+//     if (productsList && !initialProducts) {
+//       getProducts()
+//     }
+//   }, [productsList, initialProducts])
+
+//   useEffect(() => {
+//     const fetchCollection = async () => {
+//       setLoading(true)
+//       const result = await getCollection(builderConfig, {
+//         handle: collection,
+//       })
+//       setProducts(result.products)
+//       setLoading(false)
+//     }
+//     if (typeof collection === 'string' && !initialProducts) {
+//       fetchCollection()
+//     }
+//   }, [collection])
+
+//   if (loading) {
+//     return <LoadingDots />
+//   }
+//   const ProductComponent: any = process.env.IS_DEMO
+//     ? ProductCardDemo
+//     : ProductCard
+
+//   return (
+//     <Grid gap={2} width={['100%', '40%', '24%']}>
+//       {products.slice(offset, limit).map((product, i) => (
+//         <ProductComponent
+//           key={String(product.id) + i}
+//           {...(highlightCard?.index === i ? highlightCard : cardProps)}
+//           product={product}
+//         />
+//       ))}
+//     </Grid>
+//   )
+// }
+
+import { FC, useEffect, useState, useMemo } from 'react'
+import { Grid, GridProps, LoadingDots } from '@components/ui'
+import { ProductCard, ProductCardProps } from '@components/product'
 import {
   getCollection,
   getProduct,
+  searchProducts,
 } from '@lib/shopify/storefront-data-hooks/src/api/operations-builder'
 import builderConfig from '@config/builder'
-interface HighlightedCardProps extends Omit<ProductCardProps, 'product'> {
+interface HighlightedCardProps extends ProductCardProps {
   index: number
 }
 
 export interface ProductGridProps {
+  gridProps?: GridProps
   products?: ShopifyBuy.Product[]
-  productsList?: Array<{ product: string }>
+  productsList: Array<{ product: string }>
   collection?: string | any // ShopifyBuy.Collection
   offset: number
   limit: number
-  cardProps: Omit<ProductCardProps, 'product'>
+  cardProps: ProductCardProps
   highlightCard?: HighlightedCardProps
 }
 
@@ -34,6 +121,7 @@ export const ProductGrid: FC<ProductGridProps> = ({
   limit = 10,
   cardProps,
   highlightCard,
+  gridProps,
 }) => {
   const [products, setProducts] = useState(initialProducts || [])
   const [loading, setLoading] = useState(false)
@@ -41,7 +129,7 @@ export const ProductGrid: FC<ProductGridProps> = ({
   useEffect(() => {
     const getProducts = async () => {
       setLoading(true)
-      const promises = productsList!
+      const promises = productsList
         .map((entry) => entry.product)
         .filter((handle: string | undefined) => typeof handle === 'string')
         .map(
@@ -72,15 +160,12 @@ export const ProductGrid: FC<ProductGridProps> = ({
   if (loading) {
     return <LoadingDots />
   }
-  const ProductComponent: any = process.env.IS_DEMO
-    ? ProductCardDemo
-    : ProductCard
 
   return (
-    <Grid gap={2} width={['100%', '40%', '24%']}>
+    <Grid {...gridProps}>
       {products.slice(offset, limit).map((product, i) => (
-        <ProductComponent
-          key={String(product.id) + i}
+        <ProductCard
+          key={String(product.id)}
           {...(highlightCard?.index === i ? highlightCard : cardProps)}
           product={product}
         />
